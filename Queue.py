694284bcf3fd0f339cb8f007e982b0ec6529cbd4
Erfan Shahbazzadeh <erfan.shahbazzadeh.83@gmail.com>
2024-12-03 12:43:59 +0330

    Queue

---
+++ Queue.py
@@ -0,0 +1,28 @@
+class queuecalss:
+    def __init__(self,size):
+        front = 0
+        rear = 0
+        max_size = self.size
+        Queue = list()
+
+    def Enqueue(self,object):
+        if IsEmpty() == True:
+            self.Queue[self.rear]  = object
+
+    def Dequeue(self):
+        if IsFull() == True:
+            return object
+    
+    def Peek(self):
+        return object
+
+    def ReverseQueue():
+        return queue
+
+    def IsEmpty(): 
+        return boolean
+
+    def IsFull(): 
+        return boolean
+
+obj = queuecalss()
